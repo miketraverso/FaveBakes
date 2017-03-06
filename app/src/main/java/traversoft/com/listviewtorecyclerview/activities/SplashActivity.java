fbe45e24bd4697d28117624d0ee0e9c7aeac0dd1
Michael Traverso <mike@traversoft.com>
2017-03-06 00:04:41 -0500

    Add a fancy splash image. User doesn’t have to see a blank white screen.

---
+++ app/src/main/java/traversoft/com/listviewtorecyclerview/activities/SplashActivity.java
@@ -0,0 +1,16 @@
+package traversoft.com.listviewtorecyclerview.activities;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = new Intent(this, BakeryListingsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
